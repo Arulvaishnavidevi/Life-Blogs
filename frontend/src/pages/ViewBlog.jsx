2d2d7b4c20329ddbabcbb47f73ad57aa0b515873
Arulvaishnavidevi <arulvaishnavidevis@gmail.com>
2026-04-18 01:52:25 +0530

    Update ViewBlog.jsx

--- frontend/src/pages/ViewBlog.jsx
+++ frontend/src/pages/ViewBlog.jsx
@@ -105,7 +105,7 @@ const ViewBlog = () => {
                     {/* Image */}
                     {blog.image && (
                         <img
-                            src=src={blog.image}
+                            src={blog.image}
                             alt={blog.title}
                             className="w-full h-96 object-cover"
                         />
